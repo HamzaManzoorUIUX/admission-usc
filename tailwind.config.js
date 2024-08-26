42d4053263940efbda0053b9c4a7bc86b8dc49b0
Hamza Manzoor <hamzamanzoor8234@gmail.com>
2024-08-27 00:59:52 +0500

    navbar design complete

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,11 +1,13 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: [
-    "./index.html",
-    "./src/**/*.{js,ts,jsx,tsx}",
-  ],
+  content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        primary: "#1D8C6E",
+        secondary: "#E2E2E2",
+      },
+    },
   },
   plugins: [],
-}
+};
